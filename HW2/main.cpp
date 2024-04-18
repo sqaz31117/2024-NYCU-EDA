fd774c97cae940ec117b118aec05f782b5d1ce3c
Harry_NCTU <sqaz31117@gmail.com>
2024-04-18 17:42:20 +0800

    Modify/update the gain of nodes affected by move node only and not calculate cutsize anymore

--- HW2/main.cpp
+++ HW2/main.cpp
@@ -16,6 +16,8 @@ using namespace std::chrono;
 
 struct NodePosition {
     bool isLeft; // true if in L part, false if in R part
+    int gain;
+    list<int>::iterator gainIdx;
     unordered_map<int, list<int>::iterator> pos;
 };
 
@@ -33,14 +35,6 @@ vector<int> updateAnswer(int Nds) {
         else ans[i] = 1;
     }
     return ans;
-}
-
-int calculateCutsize() {
-    int cutSize = 0;
-    for (auto P : nets) {
-        if (P.first.size() != 0 && P.second.size() != 0) cutSize++;
-    }
-    return cutSize;
 }
 
 // calculate Gi = FS(i) - TS(i)
@@ -101,6 +95,53 @@ void moveNode(int nodeToMove, bool toLeft) {
     }
 }
 
+void updateGain(vector<bool>& freeCell, unordered_map<int, list<int>>& LPmax, 
+unordered_map<int, list<int>>& RPmax, int& LPidx, int& RPidx, int& lb, int& rb, int moveID) {
+    for (const int& net : nodes[moveID]) {
+        for (const int& nf : nets[net].first) {
+            if (freeCell[nf]) {
+                int gn = calculateGain(nf);
+                if (nodePositions[nf].isLeft) {
+                    LPmax[nodePositions[nf].gain].erase(nodePositions[nf].gainIdx);
+                    LPmax[gn].push_back(nf);
+                    nodePositions[nf].gain = gn;
+                    nodePositions[nf].gainIdx = --LPmax[gn].end();
+                    LPidx = max(LPidx, gn);
+                    lb = min(lb, gn);
+                }
+                else {
+                    RPmax[nodePositions[nf].gain].erase(nodePositions[nf].gainIdx);
+                    RPmax[gn].push_back(nf);
+                    nodePositions[nf].gain = gn;
+                    nodePositions[nf].gainIdx = --RPmax[gn].end();
+                    RPidx = max(RPidx, gn);
+                    rb = min(rb, gn);
+                }
+            }
+        }
+        for (const int& ns : nets[net].second) {
+            if (freeCell[ns]) {
+                int gn = calculateGain(ns);
+                if (nodePositions[ns].isLeft) {
+                    LPmax[nodePositions[ns].gain].erase(nodePositions[ns].gainIdx);
+                    LPmax[gn].push_back(ns);
+                    nodePositions[ns].gain = gn;
+                    nodePositions[ns].gainIdx = --LPmax[gn].end();
+                    LPidx = max(LPidx, gn);
+                    lb = min(lb, gn);
+                }
+                else {
+                    RPmax[nodePositions[ns].gain].erase(nodePositions[ns].gainIdx);
+                    RPmax[gn].push_back(ns);
+                    nodePositions[ns].gain = gn;
+                    nodePositions[ns].gainIdx = --RPmax[gn].end();
+                    RPidx = max(RPidx, gn);
+                    rb = min(rb, gn);
+                }
+            }
+        }
+    }
+}
 // Algorithm
     // step1 : calculate Gi = FS(i) - TS(i) ------> Done
     // step2 : move the largest gain node to another part if satisfy balance factor
@@ -112,63 +153,74 @@ void moveNode(int nodeToMove, bool toLeft) {
 // bfR = balance factor Right bound
 // Lpart = L part size
 // Rpart = R part size
-vector<int> FMalgorithm(int bfL, int bfR, int Lpart, int Rpart, int Nds) {
+// moves -> {gain, nodeID}
+void FMalgorithm(int bfL, int bfR, int Lpart, int Rpart, int Nds, vector<pair<int,int>>& moves) {
     auto start = high_resolution_clock::now();
     auto stop = high_resolution_clock::now();
     auto duration = duration_cast<milliseconds>(stop - start);
     vector<bool> freeCell(Nds, true);
     bool canMove = true;
-    vector<int> ans;
     int prevCut = INT_MAX;
+    // vector<pair<int,int>> moves(Nds);
+
+    unordered_map<int, list<int>> LPmax;
+    unordered_map<int, list<int>> RPmax;
+    int LPidx = INT_MIN;
+    int RPidx = INT_MIN;
+    int lb = INT_MAX;
+    int rb = INT_MAX;
+
+    for (int i = 0; i < Nds; i++) {
+        int gain = calculateGain(i);
+        nodePositions[i].gain = gain;
+        if (nodePositions[i].isLeft) {
+            LPmax[gain].push_back(i);
+            nodePositions[i].gainIdx = --LPmax[gain].end();
+            LPidx = max(LPidx, gain);
+            lb = min(lb, gain);
+        }
+        else {
+            RPmax[gain].push_back(i);
+            nodePositions[i].gainIdx = --RPmax[gain].end();
+            RPidx = max(RPidx, gain);
+            rb = min(rb, gain);
+        }
+    }
 
     while (canMove) {
         canMove = false;
-        int curCut = calculateCutsize();
-        cout << curCut << endl;
-        if (curCut < prevCut) {
-            ans = updateAnswer(Nds);
-            prevCut = curCut;
-        }
-        unordered_map<int, vector<int>> LPmax; LPmax.clear();
-        unordered_map<int, vector<int>> RPmax; RPmax.clear();
-        int LPidx = INT_MIN;
-        int RPidx = INT_MIN;
+        // find largest gain in left part
+        while (LPmax[LPidx].empty() && LPidx >= lb) --LPidx;
+        // find largest gain in right part
+        while (RPmax[RPidx].empty() && RPidx >= rb) --RPidx;
+        
+        if (LPidx < lb || RPidx < rb) break;
 
-        // TODO
-        for (int i = 0; i < Nds; i++) {
-            if (freeCell[i]) {
-                int gain = calculateGain(i);
-                if (nodePositions[i].isLeft) {
-                    LPmax[gain].push_back(i);
-                    LPidx = max(LPidx, gain);
-                }
-                else {
-                    RPmax[gain].push_back(i);
-                    RPidx = max(RPidx, gain);
-                }
-            }
-        }
-
-        // step2: check balance factor and chooose the best one to move
-        // int Lsize = Lgain.size();
-        // int Rsize = Rgain.size();
         if (LPidx >= RPidx) {
             if (LPidx == INT_MIN) break;
             int newLsize = Lpart+1;
             int newRsize = Rpart+1;
             if (bfL <= newRsize && newRsize <= bfR) {
-                int moveID = LPmax[LPidx][0];
+                int moveID = LPmax[LPidx].front();
+                // cout << "move " << moveID << " LPidx: " << LPidx << endl;
+                moves.push_back({LPidx, moveID});
                 moveNode(moveID, false);
-                canMove = true;
                 freeCell[moveID] = false;
+                LPmax[LPidx].erase(nodePositions[moveID].gainIdx);
+                updateGain(freeCell, LPmax, RPmax, LPidx, RPidx, lb, rb, moveID);
+                canMove = true;
                 ++Rpart;
                 --Lpart;
             }
             else if (RPidx != INT_MIN && bfL <= newLsize && newLsize <= bfR) {
-                int moveID = RPmax[RPidx][0];
+                int moveID = RPmax[RPidx].front();
+                moves.push_back({RPidx, moveID});
+                // cout << "move " << moveID << " RPidx: " << RPidx << endl;
                 moveNode(moveID, true);
-                canMove = true;
                 freeCell[moveID] = false;
+                RPmax[RPidx].erase(nodePositions[moveID].gainIdx);
+                updateGain(freeCell, LPmax, RPmax, LPidx, RPidx, lb, rb, moveID);
+                canMove = true;
                 --Rpart;
                 ++Lpart;
             }
@@ -177,27 +229,35 @@ vector<int> FMalgorithm(int bfL, int bfR, int Lpart, int Rpart, int Nds) {
             int newLsize = Lpart+1;
             int newRsize = Rpart+1;
             if (bfL <= newLsize && newLsize <= bfR) {
-                int moveID = RPmax[RPidx][0];
+                int moveID = RPmax[RPidx].front();
+                // cout << "move " << moveID << " RPidx: " << RPidx << endl;
+                moves.push_back({RPidx, moveID});
                 moveNode(moveID, true);
-                canMove = true;
                 freeCell[moveID] = false;
+                RPmax[RPidx].erase(nodePositions[moveID].gainIdx);
+                updateGain(freeCell, LPmax, RPmax, LPidx, RPidx, lb, rb, moveID);
+                canMove = true;
                 --Rpart;
                 ++Lpart;
             }
             else if (LPidx != INT_MIN && bfL <= newRsize && newRsize <= bfR) {
-                int moveID = LPmax[LPidx][0];
+                int moveID = LPmax[LPidx].front();
+                // cout << "move " << moveID << " LPidx: " << LPidx << endl;
+                moves.push_back({LPidx, moveID});
                 moveNode(moveID, false);
-                canMove = true;
                 freeCell[moveID] = false;
+                LPmax[LPidx].erase(nodePositions[moveID].gainIdx);
+                updateGain(freeCell, LPmax, RPmax, LPidx, RPidx, lb, rb, moveID);
+                canMove = true;
                 ++Rpart;
                 --Lpart;
             }
         }
         stop = high_resolution_clock::now();
         duration = duration_cast<milliseconds>(stop - start);
-        if (duration.count() / 1000 > 28) break;
+        if (duration.count() / 1000 > 27) break;
     } // end while
-    return ans;
+    return;
 }
 
 int main(int argc, char *argv[]) {
@@ -207,13 +267,21 @@ int main(int argc, char *argv[]) {
     std::cout.tie(nullptr);
 
     std::ifstream inFile(argv[1]);
-
+    // std::ifstream inFile("HW2\\lab2_student\\input2.txt.hgr");
+    std::ofstream outFile(argv[2]);
+    // std::ofstream outFile("output.txt");
+    
     if (!inFile.is_open()) {
         std::cerr << "Can't open file\n";
         return 1;
     }
+    if (!outFile.is_open()) {
+        std::cerr << "Can't open output file\n";
+        return 1;
+    }
 
     string line;
+    vector<bool> Partition;     // false -> right, true -> left
     int N = 0, M = 0, lines = 0;
 
     while(getline(inFile, line)) {
@@ -222,6 +290,7 @@ int main(int argc, char *argv[]) {
         if (N == 0 && M == 0) {
             iss >> N >> M;
             nets.resize(N);
+            Partition.resize(M);
             continue;
         }
         
@@ -229,11 +298,13 @@ int main(int argc, char *argv[]) {
             int nodeIdx = word-1;
             if (nodeIdx % 2) {   // right part
                 nets[lines].second.push_back(nodeIdx);
+                Partition[nodeIdx] = false;
                 nodePositions[nodeIdx].isLeft = false;
                 nodePositions[nodeIdx].pos[lines] = --nets[lines].second.end();
             }
             else {              // left part
                 nets[lines].first.push_back(nodeIdx);
+                Partition[nodeIdx] = true;
                 nodePositions[nodeIdx].isLeft = true;
                 nodePositions[nodeIdx].pos[lines] = --nets[lines].first.end();
             }
@@ -245,20 +316,41 @@ int main(int argc, char *argv[]) {
 
     int l = 0.45*M;
     int r = 0.55*M;
-    vector<int> ans;
+    vector<pair<int,int>> moves;
+    
     if (M % 2) {
-        ans = FMalgorithm(l, r, (M/2)+1, M/2, M);
+        FMalgorithm(l, r, (M/2)+1, M/2, M, moves);
     } else {
-        ans = FMalgorithm(l, r, M/2, M/2, M);
+        FMalgorithm(l, r, M/2, M/2, M, moves);
     }
+    
+    int s = moves.size();
+    if (s > 0) {
+        int pntV = moves[0].first;
+        int pnt = 0;
+        vector<int> cumulate(s, 0);
+        cumulate[0] = pntV;
+        for (int i = 1; i < s; ++i) {
+            cumulate[i] = moves[i].first + cumulate[i-1];
+            if (cumulate[i] > pntV) {
+                pnt = i;
+                pntV = cumulate[i];
+            }
+        }
 
-    std::ofstream outFile(argv[2]);
-    if (!outFile.is_open()) {
-        std::cerr << "Can't open output file\n";
-        return 1;
+        for (int i = 0; i <= pnt; ++i) {
+            int tmp = moves[i].second;
+            Partition[tmp] = Partition[tmp] ^ true;
+        }
+
+        for (int i = 0; i < M; ++i) {
+            outFile << Partition[i] << '\n';
+        }
     }
-    for (int i = 0; i < M; i++) {
-        outFile << ans[i] << '\n';
+    else {
+        for (int i = 0; i < M; ++i) {
+            outFile << Partition[i] << '\n';
+        }
     }
     return 0;
 }
